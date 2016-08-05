40351fb7565c30967bc829027a18156c514cc49b
Rafał Wojdyła <omeg@invisiblethingslab.com>
2016-08-05 15:08:30 +0200

    implement cleanup in the uninstaller

--- uninstaller/uninstaller.c
+++ uninstaller/uninstaller.c
@@ -28,7 +28,66 @@
  * We should not depend on QWT libraries because they are already uninstalled at this point.
  */
 
+PWSTR serviceNames[] = { L"qvideo", L"xen", L"xenagent", L"xenbus", L"xenfilt", L"xeniface", L"xennet", L"xenvif", L"xendisk", L"xenvbd" };
+
 int wmain(int argc, WCHAR *argv[])
 {
-	return 0;
+    LONG status;
+    HKEY key;
+
+    // delete QWT keys
+    status = RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"Software", 0, KEY_ALL_ACCESS, &key);
+    if (status != ERROR_SUCCESS)
+        return status;
+
+    RegDeleteTree(key, L"Invisible Things Lab"); // ignore failures in case it's already deleted
+
+    status = RegCloseKey(key);
+    if (status != ERROR_SUCCESS)
+        return status;
+
+    // delete device stack filter entries created by Xen pvdrivers
+    status = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+        L"SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e96a-e325-11ce-bfc1-08002be10318}",
+        0, KEY_ALL_ACCESS, &key);
+
+    if (status != ERROR_SUCCESS)
+        return status;
+
+    // FIXME: don't delete the whole value, there might be some other filters besides Xen (XENFILT).
+    RegDeleteValue(key, L"UpperFilters");
+
+    status = RegCloseKey(key);
+    if (status != ERROR_SUCCESS)
+        return status;
+
+    status = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+        L"SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e97d-e325-11ce-bfc1-08002be10318}",
+        0, KEY_ALL_ACCESS, &key);
+
+    if (status != ERROR_SUCCESS)
+        return status;
+
+    RegDeleteValue(key, L"UpperFilters");
+
+    status = RegCloseKey(key);
+    if (status != ERROR_SUCCESS)
+        return status;
+
+    // delete driver services, SC manager can't do it
+    status = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+        L"SYSTEM\\CurrentControlSet\\Services",
+        0, KEY_ALL_ACCESS, &key);
+
+    if (status != ERROR_SUCCESS)
+        return status;
+
+    for (int i = 0; i < ARRAYSIZE(serviceNames); i++)
+        RegDeleteTree(key, serviceNames[i]);
+
+    status = RegCloseKey(key);
+    if (status != ERROR_SUCCESS)
+        return status;
+
+    return 0;
 }
