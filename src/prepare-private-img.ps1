0109469d3dc99430569e293675d0baaaed797e92
Rafał Wojdyła <omeg@invisiblethingslab.com>
2024-05-08 17:58:19 +0200

    Initialize private.img volume
    Signed-off-by: Rafał Wojdyła <omeg@invisiblethingslab.com>

---
+++ src/prepare-private-img.ps1
@@ -0,0 +1,8 @@
+$disk1 = Get-Disk -Number 1
+$match = ($disk1.FriendlyName -eq 'XENSRC PVDISK')
+$match = $match -or ($disk1.FriendlyName -eq 'QEMU HARDDISK')
+$match = $match -and $disk1.PartitionStyle -eq 'RAW'
+if ($match) {
+    InitializeDisk -Number 1
+    New-Volume -DiskNumber 1 -DriveLetter Q -FriendlyName 'Qubes Private Image'
+}
